83dd928e8af965c9da6d41a8b391f2c803e07ff5
Damien BUTY <dam.buty@gmail.com>
2013-11-02 01:54:36 -0600

    Crude background display

--- js/invisible.js
+++ js/invisible.js
@@ -3,33 +3,58 @@ var repeat = function(n, char) {
     return Array(n + 1).join(char);
 };
 
-// The Invisible structures contains all the informations about the current game
+var load_sprite = function(sprite) {
+    var arrayed = [];
+    $.each(sprite.split("\n"), function() {
+        arrayed.push(this);
+    });
+    
+    return arrayed;
+};
+
+// The Invisible structure contains all the informations about the current game
 var Invisible = {
     Bootstrap: {
         loaded: 0,
         total: 0,
     },
-    Screen: {
-        width: 80,
-        height: 40,
-        fps: 10,
-        background: {
+    Assets: {
+        backgrounds: {
             active: 0,
             collection: []
         },
-        sprites: [],
-        queues: [],
-        draw: function() {
-            // First draw the background
+        sprites: []
+    },
+    Queue: {
+        
+    },
+    Screen: {
+        width: 80,
+        height: 30,
+        fps: 10,
+        alpha: ".",
+        pixel: function(char, x, y) {
+            var line = Invisible.Screen.content[y];
+            line = line.substr(0,x) + char + line.substr(x+1)
+            Invisible.Screen.content[y] = line;
+        },
+        draw: function(sprite, x, y) {
+            $.each(sprite, function(i, line) {
+                $.each(line.split(''), function(j, char) {
+                    if (char != " ") {
+                        Invisible.Screen.pixel(char, x + j, y + i);
+                    }
+                });
+            });
         },
         display: function() {
             var content = "";
-            $.each(Invisible.Screen.content, function(i, ligne) {
-                content += ligne + "<br/>"; 
+            $.each(Invisible.Screen.content, function(i, line) {
+                content += line + "<br/>"; 
             });
             
-            $("#ecran").empty();
-            $("#ecran").append(content);
+            $("#screen").empty();
+            $("#screen").append(content);
         },
         content: []
     },
@@ -38,28 +63,48 @@ var Invisible = {
     }
 };
 
-// Once Switched, the Machinist will execute every 1/fps second and :
-// - advance one step in the animation queues
-// - call the drawing and displaying
 var Machinist = {
     Reel: function() {  
-        Invisible.Screen.content = new Array(Invisible.Screen.height).map(function() { 
-            return repeat(Invisible.Screen.width, " ");
-        })
+        // first fill the screen with spaces
+        $.each(Array(Invisible.Screen.height), function() {
+            Invisible.Screen.content.push(repeat(Invisible.Screen.width, Invisible.Screen.alpha));
+        });
+        
+        // then get the backgrounds list
         $.ajax({
             url: "backgrounds/backgrounds.list",
             type: "GET",
             dataType: "text/plain",
             statusCode: {
                 200: function(backgrounds) {
-                    console.log(backgrounds);
+                    $.each(backgrounds.responseText.split("\n"), function(i, background) {
+                        if (background != "") {
+                            $.ajax({
+                                url: "backgrounds/" + background + ".sprite",
+                                type: "GET",
+                                dataType: "text/plain",
+                                statusCode: {
+                                    200: function(sprite) {
+                                        Invisible.Assets.backgrounds.collection.push(load_sprite(sprite.responseText));
+                                    }
+                                }
+                            });
+                        }
+                    });
                 }
             }        
         });
     },
+    // Once Switched, the Machinist will execute every 1/fps second and :
+    // - advance one step in the animation queues
+    // - draw the background
+    // - draw the objects by order of z-index
+    // - and display the screen
     Switch: function() {
-            setTimeout(function() {
-                
+            setInterval(function() {
+                console.log("tick");
+                Invisible.Screen.draw(Invisible.Assets.backgrounds.collection[Invisible.Assets.backgrounds.active], 0, 0);
+                Invisible.Screen.display();
             }, 1000/Invisible.Screen.fps);
     }
 };
