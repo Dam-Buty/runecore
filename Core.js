8d97bbc0a32264224b7b9234a54e011107e3f5fe
Damien BUTY <dam.buty@gmail.com>
2013-11-13 23:50:36 -0600

    Prototype approach, chainable functions

--- Core.js
+++ Core.js
@@ -14,6 +14,7 @@ var Game = {
             dataType: "text/plain",
             statusCode: {
                 200: function(assets) {
+                    // loads all assets in memory
                     $.each(assets.responseText.split("__ASSET__"), function(i, asset) {
                         if (asset != "") {
                             var type = asset.split("\n__BEGIN__\n")[0].split("_")[0];
@@ -26,13 +27,17 @@ var Game = {
                                 Game.Assets.sprites[titre] = { normal: sprite.split("\n__BLINK__\n")[0], blink: sprite.split("\n__BLINK__\n")[1] };
                             }
                             
-                            // Load the first background
+                            // Loads the first background
                             Core.load_bg("title");
-                            
-                            // And fire up the story!
-                            $.getScript(Game.path + "/Story.js");
                         }
                     });
+                    
+                    // And fires up the story!
+                    $.getScript(Game.path + "/Story.js", function() {
+                        Story.start();
+                    });
+                    
+                    return Game;
                 }
             }
         });
@@ -45,39 +50,80 @@ var Game = {
 
 var Core = {
     current_bg: "",
-    screen: $("#screen"),
+    screen: {
+        sel: $("#screen"),
+        pre: $("#screen pre"),
+        width: function() {
+            return this.pre.innerWidth();
+        },
+        height: function() {
+            return this.pre.innerHeight();
+        }
+    },
     sprites: {},
     start: function() {
         Game.load();
     },
     load_bg: function(bg) {
-        Core.screen.find("pre").empty().text(Game.Assets.bg[bg]);
+        Core.screen.sel.find("pre").empty().text(Game.Assets.bg[bg]);
+        Core.current_bg = bg;
+        
+        var max_width = Game.Assets.bg[bg].split("\n").sort(function (a, b) { return b.length - a.length; })[0].length;
+        
+        $("#screen pre").css("width", max_width + "ch");
     },
     load_sprite: function(options) {
         options = $.extend({
             x: 50,
             y: 50,
         }, options);
-        
-        console.log(options);
+        var sprite = Game.Assets.sprites[options.sprite];
         
         div = $("<div></div>")
             .append(
                 $("<pre></pre>")
-                .text(Game.Assets.sprites[options.sprite].normal)
-                .hover(function () {
-                    $(this).text(Game.Assets.sprites[options.sprite].blink);
-                }, function() {
-                    $(this).text(Game.Assets.sprites[options.sprite].normal);
-                })
+                .text(sprite.normal)
             )
             .css({
                 position: "absolute",
                 top: options.x + "px",
                 left: options.y + "px",
             })
+            .addClass("sprites");
         
-        Core.screen.append(div);
-        Core.sprites[options.sprite] = div;
+        Core.screen.sel.append(div);
+        Core.sprites[options.sprite] = $.extend(proto_sprite, { sel: div }, sprite);
+        
+        return Core.sprites[options.sprite];
     }
 };
+
+var proto_sprite = {
+    sel: "",
+    normal: "",
+    blink: "",
+    center: function(animate, time) {
+        if (animate) {
+            this.sel.animate({
+                top: (Core.screen.height() - this.sel.outerHeight()) / 2 + "px",
+                left: (Core.screen.width() - this.sel.outerWidth()) / 2 + "px"
+            }, time || 1000)
+        }
+        return this;
+    },
+    blinkable: function() {
+        sprite = this;
+        this.sel.find("pre")
+        .hover(function () {
+            $(this).text(sprite.blink);
+        }, function() {
+            $(this).text(sprite.normal);
+        });
+        
+        return this;
+    },
+    clickable: function(callback) {
+        this.sel.css("cursor", "pointer");
+        this.sel.click(callback);
+    }
+}
--- games/asciipong/Story.js
+++ games/asciipong/Story.js
@@ -1,6 +1,11 @@
 
-Core.load_sprite({ sprite: "start" });
-Core.sprites["start"].animate(
-    top: ($("#screen").innerHeight - Core.sprites["start"].outerHeight) / 2 + "px",
-    left: ($("#screen").innerWidth - Core.sprites["start"].outerWidth) / 2 + "px"
-);
+var Story = {
+    start: function() {
+        Core.load_sprite({ sprite: "start" })
+        .blinkable()
+        .center(true)
+        .clickable(function() {
+            console.log("rock'n'roll");
+        });
+    }
+};
